c584bc1e3ccd53a3022835cc81f232e58f5fc0fc
Mr.Xi <xxy380491563@163.com>
2016-03-02 00:32:42 +0800

    first

---
+++ XBook/XBook/config.swift
@@ -0,0 +1,13 @@
+//
+//  config.swift
+//  XBook
+//
+//  Created by Mr.Xi on 16/3/2.
+//  Copyright © 2016年 Mr.Xi. All rights reserved.
+//
+
+import Foundation
+
+let SCREEN_WIDTH = UIScreen.mainScreen().bounds.size.width
+
+let SCREEN_HEIGHT = UIScreen.mainScreen().bounds.size.height
