21546f517dad23f29b2028648e2b6ce53ddd281c
xixiaoyu <xixiaoyu@yundianjia.com>
2016-03-04 18:07:29 +0800

    添加发布书籍

---
+++ XBook/XBook/BookTitleView.swift
@@ -0,0 +1,56 @@
+//
+//  BookTitleView.swift
+//  XBook
+//
+//  Created by xxy on 16/3/4.
+//  Copyright © 2016年 Mr.Xi. All rights reserved.
+//
+
+import UIKit
+
+@objc protocol BookTitleDelegate {
+    optional func choiceCover()
+}
+
+class BookTitleView: UIView {
+
+    var BookCover:UIButton?
+    var BookNmae:JVFloatLabeledTextField?
+    var BookEditor:JVFloatLabeledTextField?
+    
+    var delegate:BookTitleDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.BookCover = UIButton(frame: CGRectMake(10, 8, 110, 141))
+        self.BookCover?.setImage(UIImage(named: "Cover"), forState: .Normal)
+        self.BookCover?.addTarget(self, action: Selector("choiceCover"), forControlEvents: .TouchUpInside)
+        
+        self.addSubview(self.BookCover!)
+        
+        self.BookNmae = JVFloatLabeledTextField(frame: CGRectMake(128, 48, SCREEN_WIDTH-128-15, 30))
+        self.BookEditor = JVFloatLabeledTextField(frame: CGRectMake(128, 78 + 40, SCREEN_WIDTH-128-15, 30))
+        
+        self.BookNmae?.placeholder = "书名"
+        self.BookEditor?.placeholder = "作者"
+        
+        self.BookNmae?.floatingLabelFont = XXYFont(14)
+        self.BookEditor?.floatingLabelFont = XXYFont(14)
+        
+        self.BookNmae?.font = XXYFont(14)
+        self.BookEditor?.font = XXYFont(14)
+        
+        self.addSubview(self.BookNmae!)
+        self.addSubview(self.BookEditor!)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not impleted")
+    }
+    
+    func choiceCover() {
+        self.delegate?.choiceCover!()
+    }
+
+}
--- XBook/XBook/pushNewBookViewController.swift
+++ XBook/XBook/pushNewBookViewController.swift
@@ -8,17 +8,27 @@
 
 import UIKit
 
-class pushNewBookViewController: UIViewController {
+class pushNewBookViewController: UIViewController, BookTitleDelegate {
 
+    var BookTitle:BookTitleView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.backgroundColor = COLOR_WHITE
+        
+        self.BookTitle = BookTitleView(frame: CGRectMake(0, 40, SCREEN_WIDTH, 160))
+        self.BookTitle?.delegate = self
+        self.view.addSubview(self.BookTitle!)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func choiceCover() {
+        printLog("choiceCover")
     }
     
     func close() {
